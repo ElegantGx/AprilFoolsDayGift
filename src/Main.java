ea1f4fe949a006f4bdc522561090926199e6ee6f
G(x) <2050230094@qq.com>
2026-04-04 01:36:05 +0800

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        try {
-            MyWindow.window();
-        } catch(Exception e) {
-            System.out.println("未知错误，请重试");
-        }
-    }
-}
